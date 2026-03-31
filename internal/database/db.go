eb8429d56e89884231c8cc8b2ee8769126c65acb
Dhegas <dhegasalkadicoxx@gmail.com>
2026-03-31 10:04:51 +0800

    feat: supabase dengan koneksi Transaction Pooler, dan membuat test ping supabase

--- internal/database/db.go
+++ internal/database/db.go
@@ -18,5 +18,15 @@ func Connect() {
 		log.Fatal("Failed to connect to Supabase DB:", err)
 	}
 
+	// Tes Ping ke database untuk memastikan koneksi benar-benar terbentuk
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Fatal("Failed to get database connection:", err)
+	}
+	if err := sqlDB.Ping(); err != nil {
+		log.Fatal("Failed to ping database:", err)
+	}
+
 	DB = db
+	log.Println("Berhasil terhubung ke Supabase DB!")
 }

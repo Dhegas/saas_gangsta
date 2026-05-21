4282a152b3194f70cd4094bce12eb3c8cdf17813
DekJus <gusdek.2005@gmail.com>
2026-05-22 03:02:17 +0800

    feat: implementasi user registrasi untuk use case role customer/partner

--- internal/domains/user/auth/dto/auth_request.go
+++ internal/domains/user/auth/dto/auth_request.go
@@ -4,6 +4,7 @@ type RegisterRequest struct {
 	Email    string `json:"email" binding:"required,email"`
 	Password string `json:"password" binding:"required,min=6"`
 	FullName string `json:"fullName" binding:"required,min=2,max=150"`
+	Role     string `json:"role"`
 }
 
 type LoginRequest struct {

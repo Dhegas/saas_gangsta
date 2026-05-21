d77584a005b2a3e9b6f8c7d1abbc8dbff30a56e0
Celestiarr <gustikadekk@gmail.com>
2026-05-22 00:16:37 +0800

    feat: menambahkan X-Tenant-IID untuk CORS allowed headers

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -33,7 +33,7 @@ func CORS(cfg *config.Config) gin.HandlerFunc {
 		c.Writer.Header().Set("Vary", "Origin")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers",
-			"Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With, X-Idempotency-Key",
+			"Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With, X-Idempotency-Key, X-Tenant-ID",
 		)
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, PATCH, DELETE")
 		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
